9cbacc6c360fc778ef7ea8e6b623ff68426a1a05
hanuas <1027577992@qq.com>
2026-06-05 18:18:43 +0800

    test: cover day duration parsing

---
+++ tests/test_days.py
@@ -0,0 +1,6 @@
+import unittest
+from duration_utils import parse_duration
+class ParseDurationDays(unittest.TestCase):
+  def test_days(self):
+    self.assertEqual(parse_duration("1d"), 86400)
+    
